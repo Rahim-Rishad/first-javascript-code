2ea17f7084e2a3b86928181a96cb758b5254d774
unknown <rishad07.cit@gmail.com>
2026-07-22 12:15:47 +0600

    first javascript code

---
+++ first.js
@@ -0,0 +1,16 @@
+console.log(7);
+console.log(3.14);
+console.log("rishad");
+
+// Variable
+var name = "Rishad";
+var age = 25;
+var isStudent = true;
+var temparature = 35;
+
+// let, const
+let price = 100;
+price = 200;
+
+let all = name + age + isStudent + temparature + price;
+console.log(all);
