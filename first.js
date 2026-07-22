d0d16089a1b5de71348dd3cc53ae744315da514b
unknown <rishad07.cit@gmail.com>
2026-07-22 13:33:25 +0600

    variable er rules add korechi

--- first.js
+++ first.js
@@ -17,3 +17,10 @@ console.log(all);
 
 let name1 = "Rubaiyat";
 let name2 = "Rahim";
+
+let name3 = "Ruhul Amin";
+console.log(name3);
+
+const pi = 3.14;
+console.log(typeof pi);
+
---
+++ variableName.js
@@ -0,0 +1,10 @@
+const her_phone_number = "123-456-7890"; //snake_case
+const herPhoneNumber = "123-456-7890"; //camelCase
+const HerPhoneNumber = "123-456-7890"; //PascalCase
+
+let herName01 = "MyMother"; //cannot start with a number.
+let $herAddress = "Dhaka"; //only use $ or _ as the first character.
+let Apple = "Red";
+let apple = "Green"; //case sensitive, so Apple and apple are different variables.
+
+console.log(her_phone_number + " " + herPhoneNumber + " " + HerPhoneNumber + " " + herName01 + " " + $herAddress + " " + Apple + " " + apple);
